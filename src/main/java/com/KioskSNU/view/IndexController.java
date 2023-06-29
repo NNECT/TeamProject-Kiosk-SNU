dacca4d72a839af98d085631af7e4582edd3e374
NNECT <wingggkr@gmail.com>
2023-06-29 09:52:56 +0900

    session map - seat와 room 구분 추가 및 인덱스 컨트롤러에 테스트 코드 작성

--- src/main/java/com/KioskSNU/view/IndexController.java
+++ src/main/java/com/KioskSNU/view/IndexController.java
@@ -3,21 +3,27 @@ package com.KioskSNU.view;
 import com.KioskSNU.secure.RSA;
 import com.KioskSNU.secure.SHA;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Controller
 public class IndexController {
     Map<Integer, HttpSession> seatMap;
+    Map<Integer, HttpSession> roomMap;
     RSA rsa;
     SHA sha;
 
     @Autowired
-    public IndexController(Map<Integer, HttpSession> seatMap, RSA rsa, SHA sha) {
+    public IndexController(@Qualifier("seatMap") ConcurrentHashMap<Integer, HttpSession> seatMap,
+                           @Qualifier("roomMap") ConcurrentHashMap<Integer, HttpSession> roomMap,
+                           RSA rsa, SHA sha) {
         this.seatMap = seatMap;
+        this.roomMap = roomMap;
         this.rsa = rsa;
         this.sha = sha;
     }
