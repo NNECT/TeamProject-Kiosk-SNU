1120958c229239bd101154e93d72bf1ece10284b
NNECT <wingggkr@gmail.com>
2023-07-06 17:52:23 +0900

    @AdminLoginRequired 어노테이션을 통한 로그인 확인 인터셉터 추가

--- src/main/java/com/KioskSNU/common/Logger.java
+++ src/main/java/com/KioskSNU/common/Logger.java
@@ -17,4 +17,9 @@ public class Logger {
     public void snuLog(JoinPoint jp) {
         System.out.println("[공통 로그] 서비스 메서드 수행 - " + jp.getSignature().getName() + "(): " + jp.getSignature().toLongString());
     }
+
+    @Before("execution(* com.KioskSNU.interceptor..*.*(..))")
+    public void interceptorLog(JoinPoint jp) {
+        System.out.println("[공통 로그] 인터셉터 메서드 수행 - " + jp.getSignature().getName() + "(): " + jp.getSignature().toLongString());
+    }
 }
---
+++ src/main/java/com/KioskSNU/interceptor/AdminLoginInterceptor.java
@@ -0,0 +1,28 @@
+package com.KioskSNU.interceptor;
+
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AdminLoginInterceptor implements HandlerInterceptor {
+    /**
+     * 관리자 페이지 로그인 확인 인터셉터
+     * 해당 컨트롤러가 AdminLoginRequired 어노테이션을 가지고 있는지 확인
+     * @return 로그인이 되어있으면, 혹은 로그인이 필요하지 않으면 true, 아니면 false
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (!(handler instanceof HandlerMethod)) return true;
+        if (((HandlerMethod) handler).getMethodAnnotation(AdminLoginRequired.class) == null) return true;
+
+        HttpSession session = request.getSession();
+        if (session.getAttribute("admin") == null) {
+            response.sendRedirect("/admin/adminlogin");
+            return false;
+        }
+        return true;
+    }
+}
