d250f1dbd3638378b94388b316e17c9b938608aa
NNECT <wingggkr@gmail.com>
2023-07-06 17:25:52 +0900

    aspect logger 추가

---
+++ src/main/java/com/KioskSNU/common/Logger.java
@@ -0,0 +1,20 @@
+package com.KioskSNU.common;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Service;
+
+@Service("logger")
+@Aspect
+public class Logger {
+    @Before("execution(* com.KioskSNU.view..*.*(..))")
+    public void viewLog(JoinPoint jp) {
+        System.out.println("[공통 로그] 뷰 메서드 수행 - " + jp.getSignature().getName() + "(): " + jp.getSignature().toLongString());
+    }
+
+    @Before("execution(* com.KioskSNU.snu..*.*(..))")
+    public void snuLog(JoinPoint jp) {
+        System.out.println("[공통 로그] 서비스 메서드 수행 - " + jp.getSignature().getName() + "(): " + jp.getSignature().toLongString());
+    }
+}
