d02cfd7d683750d33a88ef81d91ad2652831dc8f
NNECT <wingggkr@gmail.com>
2023-07-07 15:52:36 +0900

    화면별 로그인 인터셉터 추가

--- src/main/java/com/KioskSNU/interceptor/InsideLoginInterceptor.java
+++ src/main/java/com/KioskSNU/interceptor/InsideLoginInterceptor.java
@@ -7,21 +7,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class AdminLoginInterceptor implements HandlerInterceptor {
-    private static final String LOGIN_URI = "/admin/adminlogin";
+public class InsideLoginInterceptor implements HandlerInterceptor {
+    private static final String LOGIN_URI = "/inside/login";
 
     /**
-     * 관리자 페이지 로그인 확인 인터셉터
-     * 해당 컨트롤러가 AdminLoginRequired 어노테이션을 가지고 있는지 확인
+     * 내부 사용자 로그인 확인 인터셉터
+     * 해당 컨트롤러가 InsideLoginRequired 어노테이션을 가지고 있는지 확인
      * @return 로그인이 되어있으면, 혹은 로그인이 필요하지 않으면 true, 아니면 false
      */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if (!(handler instanceof HandlerMethod)) return true;
-        if (((HandlerMethod) handler).getMethodAnnotation(AdminLoginRequired.class) == null) return true;
+        if (((HandlerMethod) handler).getMethodAnnotation(InsideLoginRequired.class) == null) return true;
 
         HttpSession session = request.getSession();
-        if (session.getAttribute("admin") == null) {
+        if (session.getAttribute("author") == null) {
             response.sendRedirect(LOGIN_URI);
             return false;
         }
