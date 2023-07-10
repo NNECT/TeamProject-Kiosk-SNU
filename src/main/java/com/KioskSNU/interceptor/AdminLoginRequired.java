73b6b62521e86a6d05206e4c8195c980d39ee055
NNECT <wingggkr@gmail.com>
2023-07-10 17:28:04 +0900

    어노테이션 주석 추가

--- src/main/java/com/KioskSNU/interceptor/AdminLoginRequired.java
+++ src/main/java/com/KioskSNU/interceptor/AdminLoginRequired.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Admin 로그인이 필요한 Controller 메소드에 붙이는 Annotation.
+ * 해당 Annotation이 붙은 메소드는 AdminLoginInterceptor를 거쳐 Admin 로그인 여부를 확인하게 되며,
+ * 로그인이 되어있지 않다면 로그인 페이지로 redirect 된다.
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AdminLoginRequired {}
--- src/main/java/com/KioskSNU/interceptor/OutsideLoginRequired.java
+++ src/main/java/com/KioskSNU/interceptor/OutsideLoginRequired.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 사용자 로그인이 필요한 Outside Controller 메소드에 붙이는 Annotation.
+ * 해당 Annotation이 붙은 메소드는 OutsideLoginInterceptor를 거쳐 사용자 로그인 여부를 확인하게 되며,
+ * 로그인이 되어있지 않다면 Outside 로그인 페이지로 redirect 된다.
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface OutsideLoginRequired {}
