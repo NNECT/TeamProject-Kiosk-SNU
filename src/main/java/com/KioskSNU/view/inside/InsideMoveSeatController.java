d0eea47dea41284e0f0e4e2338554450c341587d
chbac <chbacgo@gmail.com>
2023-07-19 17:12:08 +0900

    내부키오스크 자리이동 코드정리

--- src/main/java/com/KioskSNU/view/inside/InsideMoveSeatController.java
+++ src/main/java/com/KioskSNU/view/inside/InsideMoveSeatController.java
@@ -34,7 +34,6 @@ public class InsideMoveSeatController {
     UsageCommutationTicketService usageCommutationTicketService;
 
 
-
     @Autowired
     InsideMoveSeatController(
             ConcurrentHashMap<Integer, UsageSeatDTO> seatMap,
@@ -63,7 +62,7 @@ public class InsideMoveSeatController {
         mav.setViewName("inside/inside_moveSeat");
 
         //menu에서 자리이동 눌렀을 경우 자리선택 페이지로 이동
-        if(type == null || number == null){
+        if (type == null || number == null) {
             mav.setViewName("inside/inside_select");
 
             Map<Integer, Integer> seatStatusMap = new HashMap<>();
@@ -79,58 +78,52 @@ public class InsideMoveSeatController {
             return mav;
         }
 
-        //자리이동에서 이동할 좌석 선택 이후 변경처리 진행
-        if(type.equals("seat")){
-
-            UsageSeatDTO usage = new UsageSeatDTO();
-            //usage.seatId에 / 들어온 number로 조회한 좌석의 id값 넣어주기
-            usage.setSeat_id(seatService.getBySeatNumber(number).getId());
-            //현재 사용시간 넣어주기
-            usage.setStartDateTime(LocalDateTime.now());
-            //들어온 number 번호에 usage정보 넣어주기
-            seatMap.put(number, usage);
-
-        }else {
+        if (!type.equals("seat")) {
             mav.setViewName("redirect:/inside/move");
+            return mav;
         }
 
+        //자리이동에서 이동할 좌석 선택 이후 변경처리 진행
+        UsageSeatDTO usage = new UsageSeatDTO();
+        //usage.seatId에 / 들어온 number로 조회한 좌석의 id값 넣어주기
+        usage.setSeat_id(seatService.getBySeatNumber(number).getId());
+        //현재 사용시간 넣어주기
+        usage.setStartDateTime(LocalDateTime.now());
+        //들어온 number 번호에 usage정보 넣어주기
+        seatMap.put(number, usage);
 
-        switch (type) {
-            case "seat" : {
-                // 자리 번호 확인
-                int seatNumber = (Integer) session.getAttribute("insideNumber");
-                if (!seatMap.containsKey(seatNumber)) {
-                    mav.setViewName("redirect:/inside/index");
-                    return mav;
-                }
-
-                //사용자 저장
-                AccountDTO accountDTO = (AccountDTO) session.getAttribute("author");
-
-                //자리사용종료 / 로그인기록 로그아웃
-                session.removeAttribute("author");
-                UsageSeatDTO usageSeatDTO = seatMap.get(seatNumber);
-                seatMap.remove(seatNumber);
-
-                // 자리 사용 기록 저장
-                usageSeatDTO.setEndDateTime(LocalDateTime.now());
-                if (usageSeatDTO.getId() == 0) usageSeatService.insert(usageSeatDTO);
-                else usageSeatService.update(usageSeatDTO);
-
-                // 정기권 처리
-                List<UsageCommutationTicketDTO> commutationTicketDTOList = usageCommutationTicketService.getAllByAccount(accountDTO);
-                if (
-                        !commutationTicketDTOList.isEmpty()
-                                && !commutationTicketDTOList.get(0).getEndDate().isBefore(LocalDate.now())
-                ) break;
-
-                // 사용 시간 차감
-                int usedTime = (int) ChronoUnit.MINUTES.between(usageSeatDTO.getStartDateTime(), usageSeatDTO.getEndDateTime());
-                accountDTO.setRemainTime(Math.max(accountDTO.getRemainTime() - usedTime, 0));
-                accountService.update(accountDTO);
-                break;
-            }
+        // 자리 번호 확인
+        int seatNumber = (Integer) session.getAttribute("insideNumber");
+        if (!seatMap.containsKey(seatNumber)) {
+            mav.setViewName("redirect:/inside/index");
+            return mav;
         }
+
+        //사용자 저장
+        AccountDTO accountDTO = (AccountDTO) session.getAttribute("author");
+
+        //자리사용종료 / 로그인기록 로그아웃
+        session.removeAttribute("author");
+        UsageSeatDTO usageSeatDTO = seatMap.get(seatNumber);
+        seatMap.remove(seatNumber);
+
+        // 자리 사용 기록 저장
+        usageSeatDTO.setEndDateTime(LocalDateTime.now());
+        if (usageSeatDTO.getId() == 0) usageSeatService.insert(usageSeatDTO);
+        else usageSeatService.update(usageSeatDTO);
+
+        // 정기권 처리
+        List<UsageCommutationTicketDTO> commutationTicketDTOList = usageCommutationTicketService.getAllByAccount(accountDTO);
+        if (
+                !commutationTicketDTOList.isEmpty()
+                        && !commutationTicketDTOList.get(0).getEndDate().isBefore(LocalDate.now())
+        ) return mav;
+
+        // 사용 시간 차감
+        int usedTime = (int) ChronoUnit.MINUTES.between(usageSeatDTO.getStartDateTime(), usageSeatDTO.getEndDateTime());
+        accountDTO.setRemainTime(Math.max(accountDTO.getRemainTime() - usedTime, 0));
+        accountService.update(accountDTO);
+
 
         return mav;
     }
