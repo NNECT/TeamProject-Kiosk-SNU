d042b9bfa17582d313f33784277897d0530d7bb4
NNECT <wingggkr@gmail.com>
2023-07-19 10:22:53 +0900

    내부 키오스크 퇴실 처리

--- src/main/java/com/KioskSNU/view/inside/InsideIndexController.java
+++ src/main/java/com/KioskSNU/view/inside/InsideIndexController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -40,6 +41,14 @@ public class InsideIndexController {
             return modelAndView;
         }
 
+        //테스트코드
+        session.setAttribute("author", accountService.getById(1));
+        UsageSeatDTO usageDTO = new UsageSeatDTO();
+        usageDTO.setAccount_id(1);
+        usageDTO.setSeat_id((Integer) session.getAttribute("insideNumber"));
+        usageDTO.setStartDateTime(LocalDateTime.now());
+        seatMap.put(usageDTO.getSeat_id(), usageDTO);
+
         modelAndView.setViewName("inside/index");
         return modelAndView;
     }
@@ -48,58 +57,52 @@ public class InsideIndexController {
     public ModelAndView start(HttpSession session) {
         ModelAndView modelAndView = new ModelAndView();
 
-        /*switch ((String) session.getAttribute("insideType")) {
-            case "seat": {
-                // 사용자가 등록되지 않았거나 사용 시작이 되지 않았으면 인덱스 페이지에서 대기
-                if (!seatMap.containsKey((Integer) session.getAttribute("insideNumber"))
-                        || seatMap.get((Integer) session.getAttribute("insideNumber")).getStartDateTime() == null) {
-                    modelAndView.setViewName("redirect:/inside/index");
-                    return modelAndView;
-                }
-
-                // 사용자가 등록되어있는데, 사용자 정보가 없으면 인덱스 페이지에서 대기
-                AccountDTO accountDTO = accountService.getById(seatMap.get((Integer) session.getAttribute("insideNumber")).getAccount_id());
-                if (accountDTO == null) {
-                    System.out.println(session.getAttribute("insideNumber") + "번 자리에 등록된 사용자ID " + seatMap.get((Integer) session.getAttribute("insideNumber")).getAccount_id() + "에 해당하는 사용자가 없습니다.");
-                    modelAndView.setViewName("redirect:/inside/index");
-                    return modelAndView;
-                }
-
-                session.setAttribute("author", accountDTO);
-                break;
-            }
-
-            case "room": {
-                // 사용자가 등록되지 않았거나 사용 시작이 되지 않았으면 인덱스 페이지에서 대기
-                if (!roomMap.containsKey((Integer) session.getAttribute("insideNumber"))
-                        || roomMap.get((Integer) session.getAttribute("insideNumber")).getStartDateTime() == null) {
-                    modelAndView.setViewName("redirect:/inside/index");
-                    return modelAndView;
-                }
-
-                // 사용자가 등록되어있는데, 사용자 정보가 없으면 인덱스 페이지에서 대기
-                AccountDTO accountDTO = accountService.getById(roomMap.get((Integer) session.getAttribute("insideNumber")).getAccount_id());
-                if (accountDTO == null) {
-                    System.out.println(session.getAttribute("insideNumber") + "번 방에 등록된 사용자ID " + roomMap.get((Integer) session.getAttribute("insideNumber")).getAccount_id() + "에 해당하는 사용자가 없습니다.");
-                    modelAndView.setViewName("redirect:/inside/index");
-                    return modelAndView;
-                }
-
-                session.setAttribute("author", accountDTO);
-                break;
-            }
-
-            default: {
-                modelAndView.setViewName("redirect:/inside");
-                return modelAndView;
-            }
-        }*/
-
-        //테스트코드
-        session.setAttribute("author",accountService.getById(1));
-
-
-
+//        switch ((String) session.getAttribute("insideType")) {
+//            case "seat": {
+//                // 사용자가 등록되지 않았거나 사용 시작이 되지 않았으면 인덱스 페이지에서 대기
+//                if (!seatMap.containsKey((Integer) session.getAttribute("insideNumber"))
+//                        || seatMap.get((Integer) session.getAttribute("insideNumber")).getStartDateTime() == null) {
+//                    modelAndView.setViewName("redirect:/inside/index");
+//                    return modelAndView;
+//                }
+//
+//                // 사용자가 등록되어있는데, 사용자 정보가 없으면 인덱스 페이지에서 대기
+//                AccountDTO accountDTO = accountService.getById(seatMap.get((Integer) session.getAttribute("insideNumber")).getAccount_id());
+//                if (accountDTO == null) {
+//                    System.out.println(session.getAttribute("insideNumber") + "번 자리에 등록된 사용자ID " + seatMap.get((Integer) session.getAttribute("insideNumber")).getAccount_id() + "에 해당하는 사용자가 없습니다.");
+//                    modelAndView.setViewName("redirect:/inside/index");
+//                    return modelAndView;
+//                }
+//
+//                session.setAttribute("author", accountDTO);
+//                break;
+//            }
+//
+//            case "room": {
+//                // 사용자가 등록되지 않았거나 사용 시작이 되지 않았으면 인덱스 페이지에서 대기
+//                if (!roomMap.containsKey((Integer) session.getAttribute("insideNumber"))
+//                        || roomMap.get((Integer) session.getAttribute("insideNumber")).getStartDateTime() == null) {
+//                    modelAndView.setViewName("redirect:/inside/index");
+//                    return modelAndView;
+//                }
+//
+//                // 사용자가 등록되어있는데, 사용자 정보가 없으면 인덱스 페이지에서 대기
+//                AccountDTO accountDTO = accountService.getById(roomMap.get((Integer) session.getAttribute("insideNumber")).getAccount_id());
+//                if (accountDTO == null) {
+//                    System.out.println(session.getAttribute("insideNumber") + "번 방에 등록된 사용자ID " + roomMap.get((Integer) session.getAttribute("insideNumber")).getAccount_id() + "에 해당하는 사용자가 없습니다.");
+//                    modelAndView.setViewName("redirect:/inside/index");
+//                    return modelAndView;
+//                }
+//
+//                session.setAttribute("author", accountDTO);
+//                break;
+//            }
+//
+//            default: {
+//                modelAndView.setViewName("redirect:/inside");
+//                return modelAndView;
+//            }
+//        }
 
         modelAndView.setViewName("redirect:/inside/login");
         return modelAndView;
---
+++ src/main/webapp/WEB-INF/views/inside/logout.jsp
@@ -0,0 +1,50 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=deevice-width, initial-scale=1,minimum-scale=1,maxmun-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<c:url value="/css/inside/inside_payEnd.css"/>">
+    <link href="https://cdn.jsdelivr.net/gh/sunn-us/SUITE/fonts/static/woff2/SUITE.css" rel="stylesheet">
+
+    <style>
+        body {font-family: 'SUITE', sans-serif;}
+    </style>
+    <title>snu_start_page</title>
+</head>
+<body>
+<div id="body">
+    <section id="circleWrap1">
+        <div id="bigCircle1"></div><!--원-->
+        <div id="smallCircle1"></div><!--원-->
+    </section>
+    <section id="circleWrap2">
+        <div id="bigCircle2"></div><!--원-->
+        <div id="smallCircle2"></div><!--원-->
+    </section>
+
+    <section id="allWrap"><!--전체 감싸는 박스-->
+        <img src="<c:url value="/img/inside/check.png"/>">
+        <p id="title">퇴실 처리 완료</p>
+        <p id="time">
+            <span id="MyTimer">10</span>초뒤 홈화면으로 이동합니다.
+        </p>
+
+    </section>
+</div>
+<script>
+    let time = 10;
+    let timer = setInterval(function () {
+        time--;
+        document.getElementById('MyTimer').innerText = String(time).padStart(2, '0');
+        if (time === 0) {
+            clearInterval(timer);
+            location.href = "<c:url value="/inside/index"/>";
+        }
+    }, 1000);
+</script>
+</body>
+</html>
