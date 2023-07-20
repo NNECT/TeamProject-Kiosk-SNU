7fdf18733a7e6cdae57dfeaedd1cad1fa45db858
NNECT <wingggkr@gmail.com>
2023-07-20 15:49:29 +0900

    오타 수정

--- src/main/java/com/KioskSNU/api/LockerStatus.java
+++ src/main/java/com/KioskSNU/api/LockerStatus.java
@@ -75,7 +75,7 @@ public class LockerStatus {
     /**
      * 현재 사물함 상태를 반환하는 메소드
      * @param accountDTO 정보를 얻는 사용자의 계정
-     * @return 사물함 상태를 나타내는 Map (-1: 사용 불가, 0: 사용중, 1: 사용 가능, 2: 현재 사용자의 룸)
+     * @return 사물함 상태를 나타내는 Map (-1: 사용 불가, 0: 사용중, 1: 사용 가능, 2: 현재 사용자의 사물함)
      */
     public Map<Integer, Integer> getLockerStatusMap(AccountDTO accountDTO) {
         Map<Integer, Integer> lockerStatusMap = new HashMap<>();
