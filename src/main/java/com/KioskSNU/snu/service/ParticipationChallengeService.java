0cd5e985834f207a588e22e5155bde61238b434c
NNECT <wingggkr@gmail.com>
2023-07-24 19:52:22 +0900

    ChallengeSuccessCheck api -> service interface로 전환

--- src/main/java/com/KioskSNU/snu/service/ParticipationChallengeService.java
+++ src/main/java/com/KioskSNU/snu/service/ParticipationChallengeService.java
@@ -1,5 +1,6 @@
 package com.KioskSNU.snu.service;
 
+import com.KioskSNU.snu.logic.ChallengeSuccessCheck;
 import com.KioskSNU.snu.mapper.ParticipationChallengeMapper;
 
-public interface ParticipationChallengeService extends ParticipationChallengeMapper {}
+public interface ParticipationChallengeService extends ParticipationChallengeMapper, ChallengeSuccessCheck {}
