b626308e161cc1d03ee356b72972437753bc786e
NNECT <wingggkr@gmail.com>
2023-07-26 18:37:26 +0900

    외부 키오스크 결제창 화면 처리

--- src/main/java/com/KioskSNU/view/outside/OutsidePaymentController.java
+++ src/main/java/com/KioskSNU/view/outside/OutsidePaymentController.java
@@ -1,14 +1,65 @@
 package com.KioskSNU.view.outside;
 
+import com.KioskSNU.snu.dto.*;
+import com.KioskSNU.snu.service.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class OutsidePaymentController {
-    public ModelAndView process() {
+    private final HashMap<String, Object> ticketMap;
+    private final RoomService roomService;
+
+    @RequestMapping("/outside/payment")
+    public ModelAndView process(HttpSession session) {
         ModelAndView mav = new ModelAndView();
+
+        List<String> ticketList = new ArrayList<>();
+        List<String> timeList = new ArrayList<>();
+        List<Integer> priceList = new ArrayList<>();
+
+        ticketMap.forEach((key, value) -> {
+            switch (key) {
+                case "timeTicket":
+                    ticketList.add("시간권");
+                    TimeTicketDTO timeTicket = (TimeTicketDTO) value;
+                    timeList.add(timeTicket.getTime() + "시간");
+                    priceList.add(timeTicket.getPrice());
+                    break;
+                case "commutationTicket":
+                    ticketList.add("정기권");
+                    CommutationTicketDTO commutationTicket = (CommutationTicketDTO) value;
+                    timeList.add(commutationTicket.getDay() + "일");
+                    priceList.add(commutationTicket.getPrice());
+                    break;
+                case "room":
+                    ticketList.add("룸");
+                    timeList.add(value + "시간");
+                    int roomNumber = (int) session.getAttribute("selectNumber");
+                    RoomDTO roomDTO = roomService.getByRoomNumber(roomNumber);
+                    priceList.add(((Integer) value) * roomDTO.getRoomType_price());
+                    break;
+                case "lockerTicket":
+                    ticketList.add("사물함");
+                    LockerTicketDTO lockerTicket = (LockerTicketDTO) value;
+                    timeList.add(lockerTicket.getDay() + "일");
+                    priceList.add(lockerTicket.getPrice());
+                    break;
+            }
+        });
+
+        mav.addObject("ticketList", ticketList);
+        mav.addObject("timeList", timeList);
+        mav.addObject("priceList", priceList);
+		mav.addObject("totalPrice", priceList.stream().mapToInt(Integer::intValue).sum());
         mav.setViewName("outside/payment");
         return mav;
     }
