00d604b8e2102efc2f77627c23e1e489a38ac1a4
NNECT <wingggkr@gmail.com>
2023-07-31 16:38:59 +0900

    챌린지 선택시 초기화 수정

--- src/main/java/com/KioskSNU/view/outside/OutsideChallengeController.java
+++ src/main/java/com/KioskSNU/view/outside/OutsideChallengeController.java
@@ -50,27 +50,35 @@ public class OutsideChallengeController {
         return "outside/end";
     }
 
-
     @PostMapping("/add")
     @OutsideLoginRequired
     public ModelAndView addChallenge(@RequestParam("selectedChallengeId") Integer selectedChallengeId, HttpSession session) {
         ModelAndView mav = new ModelAndView();
         try {
+            // 참여자, 챌린지 정보 가져오기
             AccountDTO accountDTO = (AccountDTO) session.getAttribute("author");
             ChallengeDTO challengeDTO = challengeService.getById(selectedChallengeId);
+
+            // 참여자, 챌린지 정보를 담은 DTO 생성
             ParticipationChallengeDTO participationChallengeDTO = new ParticipationChallengeDTO();
-            participationChallengeDTO.setAccount_id(accountDTO.getId());
-            participationChallengeDTO.setChallenge_id(selectedChallengeId);
+            participationChallengeDTO.setAccountDTO(accountDTO);
+            participationChallengeDTO.setChallengeDTO(challengeDTO);
+
+            // 챌린지 시작, 종료 시간 설정
             participationChallengeDTO.setStartDateTime(LocalDateTime.now());
             participationChallengeDTO.setEndDateTime(calTime(challengeDTO));
+
+            // 챌린지 목표 정보 설정
             participationChallengeDTO.setGoalDay(challengeDTO.getGoalDay());
             participationChallengeDTO.setGoalHour(challengeDTO.getGoalHour());
             participationChallengeDTO.setRewardPoint(challengeDTO.getRewardPoint());
-            participationChallengeDTO.setActive(isActive(participationChallengeDTO));
-            participationChallengeDTO.setResult(checkSuccess(participationChallengeDTO));
 
+            // 챌린지 참여 초기화 설정
+            participationChallengeDTO.setActive(true);
+            participationChallengeDTO.setResult(false);
+
+            // 챌린지 참여 정보 DB에 저장
             participationChallengeService.insert(participationChallengeDTO);
-
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -79,24 +87,34 @@ public class OutsideChallengeController {
 
         return mav;
     }
-    public boolean checkSuccess(ParticipationChallengeDTO participationChallengeDTO) {
-        int result = participationChallengeService.challengeSuccessCheck(participationChallengeDTO);
 
-        return result == 1;
-    }
-    public boolean isActive(ParticipationChallengeDTO participationChallengeDTO) {
-        return participationChallengeService.challengeSuccessCheck(participationChallengeDTO) == 0;
-    }
+//    /**
+//     * 챌린지 성공 여부 확인
+//     * @param participationChallengeDTO 챌린지 참여 정보 DTO
+//     * @return 챌린지 성공 여부
+//     */
+//    public boolean checkSuccess(ParticipationChallengeDTO participationChallengeDTO) {
+//        int result = participationChallengeService.challengeSuccessCheck(participationChallengeDTO);
+//
+//        return result == 1;
+//    }
 
+//    /**
+//     * 챌린지가 진행중인지 확인
+//     * @param participationChallengeDTO 챌린지 참여 정보 DTO
+//     * @return 챌린지 진행 여부
+//     */
+//    public boolean isActive(ParticipationChallengeDTO participationChallengeDTO) {
+//        return participationChallengeService.challengeSuccessCheck(participationChallengeDTO) == 0;
+//    }
+
+    /**
+     * 챌린지 종료 시간 계산.
+     * 현재 시간 + 챌린지 기간.
+     * @param challengeDTO 챌린지 정보 DTO
+     * @return 챌린지 종료 시간
+     */
     public LocalDateTime calTime(ChallengeDTO challengeDTO){
-        LocalDateTime endTime = LocalDateTime.now().plusDays(challengeDTO.getPeriodDays()).plusHours(challengeDTO.getPeriodHours());
-
-        return endTime;
+        return LocalDateTime.now().plusDays(challengeDTO.getPeriodDays()).plusHours(challengeDTO.getPeriodHours());
     }
-
-
-
-
-
-
 }
